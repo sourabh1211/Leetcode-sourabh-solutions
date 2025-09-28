540c3ac126a50d0f39d5792fd9ef2be68417c1b3
Sourabh verma <sourabhvr8482@gmail.com>
2025-09-28 17:25:02 +0530

    dfs cycle undirected

---
+++ Graph/BFS-DFS/DFS cycle detect Undirected.cpp
@@ -0,0 +1,21 @@
+bool dfs(adj,vis,node,parent){
+    vis[node]=1;
+    for(auto i : adj[node]){
+        if(!vis[node]){
+            if(dfs(adj,vis,i,node)==true)return true;
+            else if(i!=parent)return true;
+        }
+    }
+  return false;
+}
+ bool isCycle(int V, vector<int> adj[]) {
+       int vis[V] = {0}; 
+       // for graph with connected components 
+       for(int i = 0;i<V;i++) {
+           if(!vis[i]) {
+               if(dfs(i, -1, vis, adj) == true) return true; 
+           }
+       }
+       return false; 
+    }
+};
