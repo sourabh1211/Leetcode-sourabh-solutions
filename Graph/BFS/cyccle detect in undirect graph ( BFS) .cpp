45ce863c60eced184e4c8dc9eb951b2fdafc32fc
Sourabh verma <sourabhvr8482@gmail.com>
2025-09-28 17:10:32 +0530

    bfs cyclde detect

---
+++ Graph/BFS/cyccle detect in undirect graph ( BFS) .cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+ bool bfs(vector<vector<int>>adj,vector<int>visited,int nde){
+    queue<pair<int,int>>q;
+    q.push({nde,-1});
+    vis[node] = 1;
+    while(!q.empty()){
+        int node = q.top().first;
+        int parent = q.top().second;
+        q.pop();
+        for(auto i :adj[node]){
+            if(!vis[i]){
+                vis[i]=1;
+                q.push({i,node});
+            }
+            else if(i!=parent){
+                return true;
+            }
+        }
+    }
+    return false;
+ }
